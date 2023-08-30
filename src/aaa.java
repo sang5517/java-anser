79fd19eed7e8c2fb8e4bca3385eb80a92b711883
sang5517 <sang5517@nate.com>
2023-08-30 17:35:39 +0900

    ë변수 문제 풀이2

--- src/aaa.java
+++ src/aaa.java
@@ -1,4 +1,4 @@
-public class ㅇㄻㄹㅇㄻ {
+public class aaa {
     public static void main(String[] args) {
 
 
--- src/fsfsfsf.java
+++ src/fsfsfsf.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class ㅇㄹㅇㄹ {
+public class fsfsfsf {
     public static void main(String[] args) {
         Random ran = new Random();
 
